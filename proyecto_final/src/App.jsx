df4746b7c8f0b0b0c636b65bcbffd6885e971a54
Rosse Gil <2210012@ipisa.edu.do>
2025-05-02 11:50:29 -0400

    CSS  del menu

--- proyecto_final/src/App.jsx
+++ proyecto_final/src/App.jsx
@@ -1,54 +1,34 @@
+// App.jsx
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import SobreNosotros from './SobreNosotros.jsx';
 import Contacto from './Contacto.jsx';
 import Venta from './Venta.jsx';
 import Login from './Login.jsx';    
+import './App.css'; 
 
 function App() {
     return (
         <div>
-            <nav>
-                <Link to="/SobreNosotros">
-                    <h4>Sobre Nosotros</h4>
-                </Link>
-            </nav>
+            <div className="navbar">
+                <div className="logo">
+                    🧊 Refrielectric
+                </div>
+                <div className="nav-links">
+                    <Link to="/SobreNosotros">Sobre Nosotros</Link>
+                    <Link to="/Contacto">Contacto</Link>
+                    <Link to="/Venta">Venta</Link>
+                    <Link to="/Login">Login</Link>
+                </div>
+            </div>
 
             <Routes>
                 <Route path="/SobreNosotros" element={<SobreNosotros />} />
-            </Routes>
-
-            <nav>
-                <Link to="/Contacto">
-                    <h4>Contacto</h4>
-                </Link>
-            </nav>
-
-            <Routes>
                 <Route path="/Contacto" element={<Contacto />} />
-            </Routes>
-
-            <nav>
-                <Link to="/Venta">
-                    <h4>Venta</h4>
-                </Link>
-            </nav>
-
-            <Routes>
                 <Route path="/Venta" element={<Venta />} />
-            </Routes>
-
-            <nav>
-                <Link to="/Login">
-                    <h4>Login</h4>
-                </Link>
-            </nav>
-
-            <Routes>
                 <Route path="/Login" element={<Login />} />
             </Routes>
         </div>
-            
     );
 }
 
