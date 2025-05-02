bbf6e584b2654383de4962476e964e4deb8fe2c7
khaterine17 <vasquezcaterin68@gmail.com>
2025-05-02 11:21:49 -0400

    agregando contacto

--- proyecto_final/src/App.jsx
+++ proyecto_final/src/App.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import SobreNosotros from './SobreNosotros';
+import SobreNosotros from './SobreNosotros.jsx';
+import Contacto from './contacto.jsx';
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <Link to="/SobreNosotros">
-          <h4>Sobre Nosotros</h4>
-        </Link>
-      </nav>
+    return (
+        <div>
+            <nav>
+                <Link to="/SobreNosotros">
+                    <h4>Sobre Nosotros</h4>
+                </Link>
+            </nav>
 
-      <Routes>
-        <Route path="/SobreNosotros" element={<SobreNosotros />} />
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/SobreNosotros" element={<SobreNosotros />} />
+            </Routes>
+
+            <nav>
+                <Link to="/Contacto">
+                    <h4>Contacto</h4>
+                </Link>
+            </nav>
+
+            <Routes>
+                <Route path="/Contacto" element={<Contacto />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
