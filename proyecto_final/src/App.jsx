8cca8ec543687ff233e2d704282764a0db0e4162
Rosse Gil <2210012@ipisa.edu.do>
2025-05-02 11:42:57 -0400

    NavBar ready

--- proyecto_final/src/App.jsx
+++ proyecto_final/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import SobreNosotros from './SobreNosotros.jsx';
-<<<<<<< HEAD
 import Contacto from './Contacto.jsx';
 import Venta from './Venta.jsx';
-=======
-import Contacto from './contacto.jsx';
-import Login from './Login.jsx';
->>>>>>> e8f61fe5dcbec7ebfe42973245dcea5b8ef6659e
+import Login from './Login.jsx';    
 
 function App() {
     return (
@@ -31,27 +27,28 @@ function App() {
             <Routes>
                 <Route path="/Contacto" element={<Contacto />} />
             </Routes>
-<<<<<<< HEAD
 
             <nav>
                 <Link to="/Venta">
                     <h4>Venta</h4>
-=======
-            <nav>
-                <Link to="/Login">
-                    <h4>Login</h4>
->>>>>>> e8f61fe5dcbec7ebfe42973245dcea5b8ef6659e
                 </Link>
             </nav>
 
             <Routes>
-<<<<<<< HEAD
                 <Route path="/Venta" element={<Venta />} />
-=======
+            </Routes>
+
+            <nav>
+                <Link to="/Login">
+                    <h4>Login</h4>
+                </Link>
+            </nav>
+
+            <Routes>
                 <Route path="/Login" element={<Login />} />
->>>>>>> e8f61fe5dcbec7ebfe42973245dcea5b8ef6659e
             </Routes>
         </div>
+            
     );
 }
 
